25aaf5e387e6ebc394e7154bb94f4cd884039229
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-24 19:32:39 -0500

    phase one complete

---
+++ Heist Jason Scott Group/TeamMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heist_Jason_Scott_Group
+{
+    class TeamMember
+    {
+        private decimal _courageFactor;
+
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor 
+        { 
+            get
+            {
+                return _courageFactor;
+            } 
+            set 
+            {
+                if (value > 0.0m && value < 2.0m)
+                {
+                    _courageFactor = value;
+                }
+            }
+        }
+
+        public TeamMember(string name, int skill, decimal courage)
+        {
+            Name = name;
+            SkillLevel = skill;
+            _courageFactor = courage;
+        }
+
+    }
+}
